c6f512d8177c4bb333428ccf43efe5664dcb0989
JuanSanjuanLemos <juansanjuan.dev@gmail.com>
2022-02-17 10:33:25 -0300

    :construction: Formated main-time and add filter to input
    #

--- script.js
+++ script.js
@@ -1,27 +1,21 @@
-//fazer amanhã
-//fazer ficar com duas casas decimais
-//restringir valores de entrada
-// dividir melhor as funções
 const buttonInite = document.querySelector(".main-button");
 let divTime = document.querySelector(".main-time");
+
 buttonInite.addEventListener("click",function(){
-    writeTime();
-    
-});
-
-function getTimes(){
-    let hour = Number(document.getElementById("set-hour").value);
-    let minute = Number(document.getElementById("set-minute").value);
-    let second = Number(document.getElementById("set-second").value);
-    let fullTime = [hour,minute,second]
-    return fullTime;
-}
-
-function writeTime(){
     let hour = getHour();
     let minute = getMinute();
     let second = getSecond();
-    divTime.innerText = `${hour}:${minute}:${second}`;
+    mySetInterval(hour,minute,second);
+    buttonInite.disabled = true;
+});
+
+function writeTime(hour,minute,second){  
+    let timeString = addZero(hour,minute,second);
+    return divTime.innerText = `${timeString[0]}:${timeString[1]}:${timeString[2]}`;
+}
+
+function mySetInterval(hour,minute,second){
+    writeTime(hour,minute,second);
     let mySetInterval = setInterval(() => {
         second --;
         if(second<0){
@@ -35,22 +29,33 @@ function writeTime(){
         if(second === 0 && minute===0 && hour === 0){
             clearInterval(mySetInterval);
         }
-        if(second<10){
-            divTime.innerText = `${hour}:${minute}:0${second}`;
-        }else{
-            divTime.innerText = `${hour}:${minute}:${second}`;
-        }
-        if(minute<10){
-            divTime.innerText = `${hour}:0${minute}:${second}`;
-        }else{
-            divTime.innerText = `${hour}:${minute}:${second}`;
-        }
-        if(hour<10){
-            divTime.innerText = `0${hour}:${minute}:${second}`;
-        }else{
-            divTime.innerText = `${hour}:${minute}:${second}`;
-        }         
+
+        let timeString = addZero(hour,minute,second);
+        return divTime.innerText = `${timeString[0]}:${timeString[1]}:${timeString[2]}`;         
     }, 1000);
+}
+
+function addZero(hour,minute,second){
+    let hourString = String(hour);
+    let minuteString = String(minute);
+    let secondString = String(second);
+    hourString = hourString.padStart(2,"0");
+    minuteString = minuteString.padStart(2,"0");
+    secondString = secondString.padStart(2,"0");
+    
+    return [hourString,minuteString,secondString];
+}
+
+function getTimes(){
+    let hour = Number(document.getElementById("set-hour").value);
+    let minute = Number(document.getElementById("set-minute").value);
+    let second = Number(document.getElementById("set-second").value);
+    if((hour >=0 && minute <= 99)&&(minute>=0 && minute && minute < 60) && (second >= 0 && second <60)){
+        let fullTime = [hour,minute,second]
+        return fullTime;
+    }else{
+        alert("Digite um tempo válido!!!");
+    }  
 }
 
 function getHour(){
